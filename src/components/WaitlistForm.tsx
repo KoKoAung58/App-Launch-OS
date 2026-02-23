4ebb79b06fa9027777265ac7a8e04c0be8c5cdb6
Ko Ko Aung <kaung@oswego.edu>
2026-02-22 17:20:59 -0700

    deploy

--- src/components/WaitlistForm.tsx
+++ src/components/WaitlistForm.tsx
@@ -134,7 +134,7 @@ export function WaitlistForm() {
     setStatus("loading");
 
     try {
-      const res = await fetch("/", {
+      const res = await fetch("/__forms.html", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: encode({
